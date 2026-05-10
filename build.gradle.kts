ab4a95edc16411a558539a013923f2f70b7986f8
T0T11 <tomas.orellano@gmail.com>
2026-05-10 14:41:30 +0200

    refactor(): configuracion del gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import net.ltgt.gradle.errorprone.errorprone
+
 plugins {
-    id("java")
+    java
+    jacoco
+    id("net.ltgt.errorprone") version "3.1.0"
 }
 
 group = "edu.teamrocket"
@@ -13,8 +17,28 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    errorprone("com.google.errorprone:error_prone_core:2.28.0")
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.errorprone.isEnabled.set(true)
+    options.errorprone.disableWarningsInGeneratedCode.set(true)
 }
 
 tasks.test {
     useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+jacoco {
+    toolVersion = "0.8.12"
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
 }
